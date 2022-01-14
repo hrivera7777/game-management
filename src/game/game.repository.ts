d174ab4f0f41a41ea12d1bb40f51e6c15520978e
Hans Rivera <hrivera@unal.edu.co>
2022-01-14 13:58:03 -0500

    changed destructuring to spread operator

--- src/game/game.repository.ts
+++ src/game/game.repository.ts
@@ -5,12 +5,8 @@ import { Game } from './game.entity';
 @EntityRepository(Game)
 export class GamesRepository extends Repository<Game> {
   async createGame(createGameDto: CreateGameDto) {
-    const { name, genre, price, fileName } = createGameDto;
     const game = this.create({
-      name,
-      genre,
-      price,
-      fileName,
+      ...createGameDto,
     });
     await this.save(game);
     return game;
