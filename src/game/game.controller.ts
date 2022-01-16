39ac8fe2950c2802f1366ba1de32f8121391e45b
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2022-01-16 18:16:43 -0500

    added guards to sensitive game routes

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -8,16 +8,19 @@ import {
   Post,
   UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { UserRole } from 'src/auth/role.enum';
+import { Roles } from 'src/auth/decorator/roles.decorator';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
 import { Game } from './game.entity';
 import { GameService } from './game.service';
+import { RolesGuard } from 'src/auth/roles.guard';
 
 @Controller('game')
-@UseGuards(AuthGuard())
+@UseGuards(RolesGuard)
 export class GameController {
   constructor(private gamesService: GameService) {}
+  @Roles(UserRole.ADMIN)
   @Post()
   createGame(@Body() createGameDto: CreateGameDto): Promise<Game> {
     return this.gamesService.createGame(createGameDto);
@@ -27,6 +30,7 @@ export class GameController {
     return this.gamesService.getGames();
   }
 
+  @Roles(UserRole.ADMIN)
   @Patch(':id')
   updateGame(
     @Param('id') id: string,
@@ -34,6 +38,7 @@ export class GameController {
   ): Promise<Game> {
     return this.gamesService.updateGame(id, updateGameDto);
   }
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   deleteGame(@Param('id') id: string): Promise<void> {
     return this.gamesService.deleteGame(id);
