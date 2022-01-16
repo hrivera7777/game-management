4b46f9e7eb560128ecd05c9995a9003938b5f71b
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2022-01-15 23:54:25 -0500

    added autentication module in game module

--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { GameController } from './game.controller';
 import { GamesRepository } from './game.repository';
 import { GameService } from './game.service';
 
 @Module({
   controllers: [GameController],
-  imports: [TypeOrmModule.forFeature([GamesRepository])],
+  imports: [TypeOrmModule.forFeature([GamesRepository]), AuthModule],
   providers: [GameService],
 })
 export class GameModule {}
