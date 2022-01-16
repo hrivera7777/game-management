11e100964232cade68f828556351dadd5bde7535
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2022-01-15 23:53:43 -0500

    added jwt strategy and passport module in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,17 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+
 @Module({
   controllers: [AuthController],
   imports: [
     TypeOrmModule.forFeature([UsersRepository]),
     JwtModule.register({ secret: '1234', signOptions: { expiresIn: 3600 } }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
