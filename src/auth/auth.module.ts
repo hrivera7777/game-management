f74a7220d0e1f8af5eb3687eb603ae220f6b24b2
Luis Eduardo Ocampo Wilches <locampow@unal.edu.co>
2022-01-15 22:38:55 -0500

    added json web token module and set it up

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,13 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   controllers: [AuthController],
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UsersRepository]),
+    JwtModule.register({ secret: '1234', signOptions: { expiresIn: 3600 } }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}
