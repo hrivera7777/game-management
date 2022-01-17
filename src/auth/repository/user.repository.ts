2119b6138b9664c52cc9a50b93874632dcd2223d
Hans Rivera <hrivera@unal.edu.co>
2022-01-16 20:07:49 -0500

    Added role as a parameter in the create method

--- src/auth/repository/user.repository.ts
+++ src/auth/repository/user.repository.ts
@@ -2,38 +2,28 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from './../entity/user.entity';
 import { SignUpDto } from './../dto/signup.dto';
 import { InternalServerErrorException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
-import { UserRole } from './../role.enum';
-import { RolesRepository } from './role.repository';
 import { Role } from './../entity/role.entity';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  @InjectRepository(RolesRepository) private rolesRepository: RolesRepository;
-  async createUser(signUpDto: SignUpDto) {
+  async createUser(signUpDto: SignUpDto, role: Role) {
     const { password } = signUpDto;
     const salt: string = await bcrypt.genSalt();
     const hashedPassword: string = await bcrypt.hash(password, salt);
-    const roleName: string = UserRole.ADMIN;
+    const user: User = this.create({
+      ...signUpDto,
+      password: hashedPassword,
+      role,
+    });
+
     try {
-      const role: Role[] = await this.rolesRepository.find();
-      console.log(JSON.stringify(role));
-      // const user: User = this.create({
-      //   ...signUpDto,
-      //   password: hashedPassword,
-      //   role,
-      // });
+      await this.save(user);
     } catch (error) {
-      console.log(JSON.stringify(error));
+      if (error.code === '23505') {
+        //code when the username or email exists
+        throw new InternalServerErrorException('Unexpected error occur');
+      }
     }
-    // try {
-    //   await this.save(user);
-    // } catch (error) {
-    //   if (error.code === '23505') {
-    //     //code when the username or email exists
-    //     throw new InternalServerErrorException('Unexpected error occur');
-    //   }
-    // }
   }
 }
