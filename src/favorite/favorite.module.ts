f8493fc28844ee74f52b90eba0f73dfd6eb55f96
Hans Rivera <hrivera@unal.edu.co>
2022-01-21 18:32:53 -0500

    imported typeorm and auth module

--- src/favorite/favorite.module.ts
+++ src/favorite/favorite.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { FavoriteController } from './favorite.controller';
 import { FavoriteService } from './favorite.service';
+import { FavoriteController } from './favorite.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FavoritesRepository } from './favorite.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([FavoritesRepository]), AuthModule],
   controllers: [FavoriteController],
-  providers: [FavoriteService]
+  providers: [FavoriteService],
 })
 export class FavoriteModule {}
