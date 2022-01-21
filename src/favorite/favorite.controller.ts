c4397fb4d316b036f221f007305492581063748d
Hans Rivera <hrivera@unal.edu.co>
2022-01-21 18:34:20 -0500

    created outline to use a CRUD

--- src/favorite/favorite.controller.ts
+++ src/favorite/favorite.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { FavoriteService } from './favorite.service';
+import { CreateFavoriteDto } from './dto/create-favorite.dto';
+import { UpdateFavoriteDto } from './dto/update-favorite.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { Favorite } from './entities/favorite.entity';
+import { User } from 'src/auth/entity/user.entity';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
 
+@UseGuards(AuthGuard())
 @Controller('favorite')
-export class FavoriteController {}
+export class FavoriteController {
+  constructor(private readonly favoriteService: FavoriteService) {}
+
+  @Post()
+  create(
+    @Body() createFavoriteDto: CreateFavoriteDto,
+    @GetUser() user: User,
+  ): Promise<Favorite> {
+    return this.favoriteService.createFavorite(createFavoriteDto, user);
+  }
+
+  @Get()
+  findAll() {
+    return this.favoriteService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.favoriteService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateFavoriteDto: UpdateFavoriteDto,
+  ) {
+    return this.favoriteService.update(+id, updateFavoriteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.favoriteService.remove(+id);
+  }
+}
--- src/favorite/favorite.service.ts
+++ src/favorite/favorite.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/entity/user.entity';
+import { CreateFavoriteDto } from './dto/create-favorite.dto';
+import { UpdateFavoriteDto } from './dto/update-favorite.dto';
+import { Favorite } from './entities/favorite.entity';
+import { FavoritesRepository } from './favorite.repository';
 
 @Injectable()
-export class FavoriteService {}
+export class FavoriteService {
+  constructor(
+    @InjectRepository(FavoritesRepository)
+    private favoritesRepository: FavoritesRepository,
+  ) {}
+  createFavorite(
+    createFavoriteDto: CreateFavoriteDto,
+    user: User,
+  ): Promise<Favorite> {
+    return this.favoritesRepository.createFavorite(createFavoriteDto, user);
+  }
+
+  findAll() {
+    return `This action returns all favorite`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} favorite`;
+  }
+
+  update(id: number, updateFavoriteDto: UpdateFavoriteDto) {
+    return `This action updates a #${id} favorite`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} favorite`;
+  }
+}
