9a92bd34a5ac157c5893cc9d14146218281de42e
Hans Rivera <hrivera@unal.edu.co>
2022-01-21 22:49:51 -0500

    create report module
    imported game, auth, and typeorm modules

---
+++ src/report/report.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ReportController } from './report.controller';
+import { ReportService } from './report.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GameModule } from 'src/game/game.module';
+
+@Module({
+  imports: [AuthModule, TypeOrmModule.forFeature(), GameModule],
+  controllers: [ReportController],
+  providers: [ReportService],
+})
+export class ReportModule {}
