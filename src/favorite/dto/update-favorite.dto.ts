161066c69c69b23fe98d63ca153530e6585e9c5e
Hans Rivera <hrivera@unal.edu.co>
2022-01-21 19:36:43 -0500

    created update favorite Dto

--- src/favorite/dto/update-favorite.dto.ts
+++ src/favorite/dto/update-favorite.dto.ts
@@ -1,1 +1,15 @@
-export class UpdateFavoriteDto {}
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { Game } from 'src/game/game.entity';
+
+export class UpdateFavoriteDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  name: string;
+  @IsOptional()
+  @IsString()
+  description: string;
+  @IsNotEmpty()
+  @IsOptional()
+  games: Game[];
+}
